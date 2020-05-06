f8050369c81386bcd04ddc89510e411b7a567272
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-06 17:54:56 -0300

    database correct updated_at

--- src/database/migrations/1588796779710-CreateTransactions.ts
+++ src/database/migrations/1588796779710-CreateTransactions.ts
@@ -34,7 +34,7 @@ export default class CreateTransactions1588796779710
             default: 'now()',
           },
           {
-            name: 'update_at',
+            name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
           },
